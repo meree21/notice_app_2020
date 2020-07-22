1aced7dbf25677e7a72b6f3acf631370c88addde
kimmeree21@gmail.com <kimmeree21@gmail.com>
2020-07-23 00:19:07 +0900

    dbconn

---
+++ src/config/dbconfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+    user : process.env.NODE_ORCLEDB_USER || "tbl", 
+    password : process.env.NODE_ORCLEDB_PASSWOR || "1234",
+    connectString : process.env.NODE_ORCLEDB_CONNECTIONSTRING || "localhost/orcl"
+    };
+
+
