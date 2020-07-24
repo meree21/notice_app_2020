96729d1efdb115069819153890f1618940b4a17b
meree21 <67742880+meree21@users.noreply.github.com>
2020-07-24 12:38:40 +0900

    Update dbconfig.js

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -1,7 +1,62 @@
-module.exports = {
-    user : process.env.NODE_ORCLEDB_USER || "tbl", 
-    password : process.env.NODE_ORCLEDB_PASSWOR || "1234",
-    connectString : process.env.NODE_ORCLEDB_CONNECTIONSTRING || "localhost/orcl"
-    };
+//route singup 파일
 
+import React, { useState } from 'react';
 
+function SingUp() {
+  const [state, setState] = useState({
+    id : "",
+    name: "",
+    list: [],
+  });
+
+  function handleChange(e) {
+    const {value} = e.target;
+    setState({
+      name: value,
+      list: state.list,
+    })
+  }
+  
+  function handleClick(){
+    const {name} = state;
+    localStorage.setItem('name', name);//로컬에 아이디를 넣어줌
+    showAllList() // 콘솔로 불러온다.
+  }
+
+  function checkSession(){
+    const session = localStorage.getItem();
+    console.log(session);
+  }
+
+  // 모든 localStorage 리스트 가져오기
+  function showAllList(){
+    console.log(localStorage.length);
+    for(let i = 0 ; i < localStorage.length ; i++){
+      console.log(localStorage[localStorage.key(i)]);
+    }
+  }
+
+  function handleCheck(){
+    const {list, name} = state;
+    localStorage.setItem('name',name);
+    setState({
+      list: list.concat({name: localStorage.getItem('name')}),
+      name: "",
+    })
+    console.log("=========================");
+    for(let i = 0; i < list.length; i++){
+      console.log(list[i].name);
+    }
+    console.log("=========================");
+  }
+
+  
+
+    return (
+        <div>
+          이름 : <input value={state.name} onChange={handleChange}/> <a onClick={handleCheck}>등록</a>
+        </div>
+      );
+    }
+
+export default SingUp;
