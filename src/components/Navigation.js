2eeeb2e9f7ff4474e5d5a9505941bd4c62eff637
meree21 <67742880+meree21@users.noreply.github.com>
2020-07-24 12:39:41 +0900

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navigation.css";
 
-function Navigation() {
+function Navigation(...props) {
   return (
     <div className="nav">
       <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <Link to="/singup">Sing Up</Link>
       <Link to="/notice">notice</Link>
     </div>
   );
